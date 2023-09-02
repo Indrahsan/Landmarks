b0c75d05402dd6d0811f591d4fb6a7681287c9f0
indrahsan <hasanindra71@gmail.com>
2023-09-02 11:00:20 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Indra Hasan on 02/09/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
