125e420d69a529a725fe4da0182f9d5b28dc6629
indrahsan <hasanindra71@gmail.com>
2023-09-03 09:20:14 +0700

    first commit

---
+++ Landmarks/View/LandmarkList.swift
@@ -0,0 +1,35 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Indra Hasan on 02/09/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView{
+                List(landmarks) { landmark in
+                    NavigationLink{
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                    
+            }
+                .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+                    LandmarkList()
+                        .previewDevice(PreviewDevice(rawValue: deviceName))
+                        .previewDisplayName(deviceName)
+                }
+    }
+}
